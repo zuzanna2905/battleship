078f81ee8931f005736d47394159e2d09d1f110d
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-06 21:03:02 +0200

    bleble

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let table = document.getElementById("ownTable");
 let rivalTable = document.getElementById("rivalsTable");
+let direction = null;
 
 if (table != null) {
     for (var i = 0; i < table.rows.length; i++) {
@@ -20,6 +21,7 @@ if (rivalTable != null) {
         rivalTable.rows[i].cells[j].onclick = function () {
             this.style.backgroundColor = 'grey';
             if(isHidden(this))  {  
+                direction = null;
                 hitOthers(this);
                 this.style.backgroundColor = 'green';
                 this.setAttribute("hitten", "true");
@@ -33,46 +35,54 @@ function hitOthers(tableCell) {
     let x = parseInt(getX(tableCell));
     let y = parseInt(getY(tableCell));
     if(x > 1 && y > 1) {
-        rivalTable.rows[y-1].cells[x-1].style.backgroundColor = 'grey';
-        rivalTable.rows[y-1].cells[x-1].setAttribute("hitten", "true");
+        setCell(x-1, y-1);
     }
     if(x > 1 && y < 10){
-        rivalTable.rows[y+1].cells[x-1].style.backgroundColor = 'grey';
-        rivalTable.rows[y+1].cells[x-1].setAttribute("hitten", "true");
+        setCell(x-1, y+1);
     }
     if(x < 10 && y > 1){
-        rivalTable.rows[y-1].cells[x+1].style.backgroundColor = 'grey';
-        rivalTable.rows[y-1].cells[x+1].setAttribute("hitten", "true");
+        setCell(x+1, y-1);
     }
     if(x < 10 && y < 10) {
-        rivalTable.rows[y+1].cells[x+1].style.backgroundColor = 'grey';
-        rivalTable.rows[y+1].cells[x+1].setAttribute("hitten", "true");
+        setCell(x+1, y+1);
     }
     checkIsWholeShip(x,y);
 }
 
 function checkIsWholeShip(x, y){
     if(checkIsAll(x,y)){
-        if(x > 1 && !isHidden(rivalTable.rows[y].cells[x-1])) {
-            rivalTable.rows[y].cells[x-1].style.backgroundColor = 'grey';
+        console.log(x,y, direction)
+        if(x > 1 && 
+        !isHidden(rivalTable.rows[y].cells[x-1]) && 
+        (direction === null || direction === 'hor') && 
+        !rivalTable.rows[y].cells[x-1].hasAttribute('hitten')) {
+            direction = 'hor';
+            checkIsWholeShip(x-1, y);
+            setCell(x-1, y);
         }
-        if(y > 1 && !isHidden(rivalTable.rows[y-1].cells[x])) {
-            rivalTable.rows[y-1].cells[x].style.backgroundColor = 'grey';
+        if(y > 1 && !isHidden(rivalTable.rows[y-1].cells[x]) && (direction === null || direction === 'ver') && ! rivalTable.rows[y-1].cells[x].hasAttribute('hitten')) {
+            direction = 'ver';
+            checkIsWholeShip(x, y-1);
+            setCell(x, y-1);
         }
-        if(y < 10  && !isHidden(rivalTable.rows[y+1].cells[x])){
-            rivalTable.rows[y+1].cells[x].style.backgroundColor = 'grey';
+        if(y < 10  && !isHidden(rivalTable.rows[y+1].cells[x]) && (direction === null || direction === 'ver') && ! rivalTable.rows[y+1].cells[x].hasAttribute('hitten')){
+            direction = 'ver';
+            checkIsWholeShip(x, y+1);
+            setCell(x, y+1);
         }
-        if(x < 10  && !isHidden(rivalTable.rows[y].cells[x+1])) {
-            rivalTable.rows[y].cells[x+1].style.backgroundColor = 'grey';
+        if(x < 10  && !isHidden(rivalTable.rows[y].cells[x+1]) && (direction === null || direction === 'hor') && ! rivalTable.rows[y].cells[x+1].hasAttribute('hitten')){
+            direction = 'hor';
+            checkIsWholeShip(x+1, y);
+            setCell(x+1, y);
         }
     }
 }
 
-function colorFields(tab) {
-    for (var i = 0; i < tab.length; i++) {
-        rivalTable.rows[tab[i].x].cells[tab[i].y].onclick = function () {
-            this.style.backgroundColor = 'grey';
-        };
+function setCell(x, y) {
+    if(!rivalTable.rows[y].cells[x].hasAttribute('hitten')){
+        rivalTable.rows[y].cells[x].style.backgroundColor = 'grey';
+        rivalTable.rows[y].cells[x].setAttribute("hitten", "true");
+        rivalTable.rows[y].cells[x].style.cursor = 'unset';
     }
 }
 
